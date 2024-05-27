0f79a41fbf2f0eb1d679b03d56b418e2ed766b6f
Aline Sayuri Kadekaro <aline.ask@gmail.com>
2024-05-26 22:26:18 -0300

    Initial commit

---
+++ app/src/main/java/br/com/collab/TelaCadastro.kt
@@ -0,0 +1,12 @@
+package br.com.collab
+
+import android.os.Bundle
+
+
+class TelaCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela_cadastro)
+    }
+
+}
---
+++ app/src/main/java/br/com/collab/TelaLogin.kt
@@ -0,0 +1,12 @@
+package br.com.collab
+
+import android.os.Bundle
+
+
+class TelaLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela_login)
+        }
+
+    }
