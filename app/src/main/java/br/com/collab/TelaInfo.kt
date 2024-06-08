b0dc58fecaa75216ec51a1434af7ea76a1430b57
Aline Sayuri Kadekaro <aline.ask@gmail.com>
2024-06-08 17:59:46 -0300

    rev cod1

--- app/src/main/java/br/com/collab/TelaInfo.kt
+++ app/src/main/java/br/com/collab/TelaInfo.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class TelaInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.tela_info
+        setContentView(R.layout.tela_info)
     }
 }
